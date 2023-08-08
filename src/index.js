6b49f82f8c4dd16381e76d8ecaa6bc712b47a85f
Will <willbickham3@gmail.com>
2023-08-08 14:19:36 -0400

    Import and call inputButton

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Hello')
+// API Key: a732470edfbc4cbdb9220158230708 http://api.weatherapi.com/v1/current.json?key=a732470edfbc4cbdb9220158230708&q=London
+import inputButton from "./interface";
+
+inputButton();
+// fetch('http://api.weatherapi.com/v1/marine.json?key=a732470edfbc4cbdb9220158230708&q=London', { mode: 'cors' })
+// .then(function(response) {
+//     return response.json();
+// })
+// .then(function(response) {
+//     console.log(response)
+// })
+// console.log('Hello')
