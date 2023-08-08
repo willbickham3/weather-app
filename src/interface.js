e73f7ba3977fe0869c8615dda4d0d5b54e8f208b
Will <willbickham3@gmail.com>
2023-08-08 14:20:11 -0400

    Functions that search weatherAPI and return/append location & temp

---
+++ src/interface.js
@@ -0,0 +1,46 @@
+function createAnElement(element, classList, innerHTML) {
+    const newElement = document.createElement(`${element}`);
+    newElement.classList.add(`${classList}`);
+    newElement.innerHTML = `${innerHTML}`;
+    return newElement
+}
+
+export default function inputButton() {
+    const mainContent = document.querySelector('.main-content');
+
+    const locationInp = document.createElement('input');
+    locationInp.type = 'text';
+    locationInp.classList.add('location-input');
+    locationInp.setAttribute('id', 'input');
+
+    const searchBtn = document.createElement('button');
+    searchBtn.classList.add('location-input');
+    searchBtn.setAttribute('id', 'button');
+    searchBtn.innerHTML = 'Search';
+    searchBtn.addEventListener('click', clickSearch)
+
+    mainContent.append(locationInp, searchBtn);
+}
+
+ async function clickSearch() {
+    const input = document.querySelector('#input');
+    let search = input.value;
+    let weather = await getWeather(search);
+    populatePage(weather.loc, weather.temperature);
+}
+
+async function getWeather(location) {
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=a732470edfbc4cbdb9220158230708&q=${location}`, { mode: 'cors' });
+    const weather = await response.json();
+    console.log(weather.current.temp_f);
+    let temperature = weather.current.temp_f;
+    let loc = weather.location.name;
+    return {temperature, loc}
+}
+
+function populatePage(location, temperature) {
+    const mainContent = document.querySelector('.main-content');
+    const locationDiv = createAnElement('h1', 'location', `${location}`);
+    const temperatureDiv = createAnElement('div', 'temperature', `${temperature}° F`);
+    mainContent.prepend(locationDiv, temperatureDiv);
+}
