0a650712626bad2044e0d13f5eb50bb24b44ab15
Will <willbickham3@gmail.com>
2023-08-08 15:12:32 -0400

    Import stylesheet

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // API Key: a732470edfbc4cbdb9220158230708 http://api.weatherapi.com/v1/current.json?key=a732470edfbc4cbdb9220158230708&q=London
 import inputButton from "./interface";
+import './style.css'
 
 inputButton();
 // fetch('http://api.weatherapi.com/v1/marine.json?key=a732470edfbc4cbdb9220158230708&q=London', { mode: 'cors' })
