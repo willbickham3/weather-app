abbdfec8090222d0d5d0be4f419439c034074de1
Will <willbickham3@gmail.com>
2023-08-09 18:29:49 -0400

    Another attempt

--- src/handleWeather/CurrentWeather.js
+++ src/handleWeather/CurrentWeather.js
@@ -1,5 +1,5 @@
 export default async function getCurrentWeather(weatherlocation) {
-    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=a732470edfbc4cbdb9220158230708&q=${weatherlocation}`, {mode: 'cors'});
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=a732470edfbc4cbdb9220158230708&q=${weatherlocation}`, {mode: "cors"});
     const weather = await response.json();
     let current = weather.current;
     let location = weather.location;
