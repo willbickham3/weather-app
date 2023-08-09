45b579faefdf21b8c028db272c262ba82053eaf7
Will <willbickham3@gmail.com>
2023-08-09 16:14:06 -0400

    Weather handlers

---
+++ src/handleWeather/CurrentWeather.js
@@ -0,0 +1,9 @@
+export default async function getCurrentWeather(weatherlocation) {
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=a732470edfbc4cbdb9220158230708&q=${weatherlocation}`, { mode: 'cors' });
+    const weather = await response.json();
+    let current = weather.current;
+    let location = weather.location;
+    console.log(current);
+    console.log(location);
+    return {current, location}
+}
---
+++ src/handleWeather/forecast.js
@@ -0,0 +1,6 @@
+export default async function getForecast(weatherlocation) {
+    const response = await fetch (`http://api.weatherapi.com/v1/forecast.json?key=a732470edfbc4cbdb9220158230708&q=${weatherlocation}&days=3`, {mode: 'cors'});
+    const forecast = await response.json();
+    console.log(forecast);
+    return forecast
+}
