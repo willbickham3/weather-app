c2f21c2f31cf83b5ac92be083cbc3de1cf3fdaba
Will <willbickham3@gmail.com>
2023-08-09 16:16:45 -0400

    Change http to https

--- src/handleWeather/forecast.js
+++ src/handleWeather/forecast.js
@@ -1,5 +1,5 @@
 export default async function getForecast(weatherlocation) {
-    const response = await fetch (`http://api.weatherapi.com/v1/forecast.json?key=a732470edfbc4cbdb9220158230708&q=${weatherlocation}&days=3`, {mode: 'cors'});
+    const response = await fetch (`https://api.weatherapi.com/v1/forecast.json?key=a732470edfbc4cbdb9220158230708&q=${weatherlocation}&days=3`, {mode: 'cors'});
     const forecast = await response.json();
     console.log(forecast);
     return forecast
