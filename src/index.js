cff5f77ac2a9962e9050a0d0e8dece95333e121d
Will <willbickham3@gmail.com>
2023-08-10 15:29:31 -0400

    Remove unused/commented code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
-// API Key: a732470edfbc4cbdb9220158230708 http://api.weatherapi.com/v1/current.json?key=a732470edfbc4cbdb9220158230708&q=London
+// API Key: a732470edfbc4cbdb9220158230708
 import inputButton from "./interface";
 import './style.css'
 
 inputButton();
-// fetch('http://api.weatherapi.com/v1/marine.json?key=a732470edfbc4cbdb9220158230708&q=London', { mode: 'cors' })
-// .then(function(response) {
-//     return response.json();
-// })
-// .then(function(response) {
-//     console.log(response)
-// })
-// console.log('Hello')
