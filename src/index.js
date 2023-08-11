584192ec11fa9b0b80017416f32b77894ac23fae
Will <willbickham3@gmail.com>
2023-08-11 13:21:16 -0400

    Call populatePage()

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 // API Key: a732470edfbc4cbdb9220158230708
 import getCurrentWeather from "./handleWeather/CurrentWeather";
 import getForecast from "./handleWeather/forecast";
-import inputButton from "./pageHandlers/interface";
+import inputButton, { populatePage } from "./pageHandlers/interface";
 import './style.css'
+import format from "date-fns/format";
 
+populatePage();
 inputButton();
-console.log(getCurrentWeather('navarre'));
-console.log(getForecast('navarre'));
+console.log(getCurrentWeather('navarre fl'));
+console.log(getForecast('navarre fl'));
+
