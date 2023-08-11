be3962049c33ee34adf67357d8adc2bada23676c
Will <willbickham3@gmail.com>
2023-08-10 22:57:28 -0400

    Add id support

--- src/components/createAnElement.js
+++ src/components/createAnElement.js
@@ -1,6 +1,12 @@
-export default function createAnElement(element, classList, innerHTML) {
+export default function createAnElement(element, classList, id, innerHTML) {
     const newElement = document.createElement(`${element}`);
     newElement.classList.add(`${classList}`);
     newElement.innerHTML = innerHTML;
+    if (id === null) {
+        id = ''
+    }
+    else {
+        newElement.setAttribute('id', id)
+    }
     return newElement
 }
