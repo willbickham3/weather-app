936669d663bfd7adf2eebfe64d905db5122ddf25
Will <willbickham3@gmail.com>
2023-08-10 22:59:59 -0400

    Give weatherSummary an argument; Change weatherCard arguments

--- src/pageHandlers/weatherSummary.js
+++ src/pageHandlers/weatherSummary.js
@@ -2,13 +2,11 @@ import getCurrentWeather from "../handleWeather/CurrentWeather";
 import getForecast from "../handleWeather/forecast";
 import weatherCard from "../components/weatherCard";
 
-export default async function weatherSummary() {
-    const input = document.querySelector('#input');
-    let search = input.value;
+export default async function weatherSummary(search) {
     let weather = await getCurrentWeather(search);
     let forecast = await getForecast(search);
     let highLow = `High: ${forecast.forecast.forecastday[0].day.maxtemp_f} | Low: ${forecast.forecast.forecastday[0].day.mintemp_f}`;
     if (weather.location.country == 'United States of America')
     {weather.location.country = weather.location.region};
-    weatherCard(weather.location.name, weather.location.country, weather.current.temp_f, highLow);
+    weatherCard(weather.location.name, weather.location.country, 'weather-summary', `${weather.current.temp_f}° F`, highLow);
 }
