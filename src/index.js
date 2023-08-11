d73f157013352b8e6c0ddbe1dc415b14840ca4e6
Will <willbickham3@gmail.com>
2023-08-10 22:56:06 -0400

    Add console logging for looking at properties

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 // API Key: a732470edfbc4cbdb9220158230708
+import getCurrentWeather from "./handleWeather/CurrentWeather";
+import getForecast from "./handleWeather/forecast";
 import inputButton from "./pageHandlers/interface";
 import './style.css'
 
 inputButton();
+console.log(getCurrentWeather('navarre'));
+console.log(getForecast('navarre'));
