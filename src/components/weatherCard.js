595ab5e192108fd9f2159595ecf515d7f39b0511
Will <willbickham3@gmail.com>
2023-08-12 14:15:30 -0400

    Refactor to be more reusable

--- src/components/weatherCard.js
+++ src/components/weatherCard.js
@@ -1,15 +1,16 @@
 import createAnElement from "./createAnElement";
 
-    export default function weatherCard(heading, heading2, id, content, footing) {
+    export default function weatherCard(heading, content, footing, id) {
         const weatherContainer = document.querySelector('.weather-container');
 
         const container = createAnElement('div', 'sub-container', null, null);
         const headingDiv = createAnElement('h1', 'weather-heading', null, `${heading}`);
-        const subHeadingDiv = createAnElement('h2', 'weather-sub-heading', null, `${heading2}`)
-        const contentDiv = createAnElement('div', 'weather-content', id, `${content}`);
-        const footingDiv = createAnElement('div', 'weather-footing', null, `${footing}`)
+        const contentDiv = createAnElement('div', 'weather-content', id, null);
+        const footingDiv = createAnElement('div', 'weather-footing', null, `${footing}`);
 
-        container.append(headingDiv, subHeadingDiv, contentDiv, footingDiv);
+        contentDiv.append(content);
+
+        container.append(headingDiv, contentDiv, footingDiv);
         weatherContainer.append(container);
         return container
     }
