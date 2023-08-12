3b05494bf8a351158bb9354e58d2be39fe381dd2
Will <willbickham3@gmail.com>
2023-08-12 14:20:15 -0400

    Refactor to match new weatherCard

--- src/pageHandlers/weatherSummary.js
+++ src/pageHandlers/weatherSummary.js
@@ -2,6 +2,7 @@ import getCurrentWeather from "../handleWeather/CurrentWeather";
 import getForecast from "../handleWeather/forecast";
 import weatherCard from "../components/weatherCard";
 import format from "date-fns/format";
+import createAnElement from "../components/createAnElement";
 
 export default async function weatherSummary(search) {
     let weather = await getCurrentWeather(search);
@@ -10,8 +11,19 @@ export default async function weatherSummary(search) {
     let newdate = new Date(weather.current.last_updated);
     let formattedDate = format(newdate, "HH:mm");
 
+    let content = createAnElement('div', 'weather-summary', null, '');
+    const one = createAnElement('div', 'content-container', null, `As of ${formattedDate}`);
+    const two = createAnElement('div', 'content-container', null, `${weather.current.temp_f}° F`);
+    const three = createAnElement('div', 'content-container', null, 'this will have content');
+
+    content.append(one, two, three)
+
     let highLow = `High: ${forecast.forecast.forecastday[0].day.maxtemp_f} | Low: ${forecast.forecast.forecastday[0].day.mintemp_f}`;
+
     if (weather.location.country == 'United States of America')
     {weather.location.country = weather.location.region};
-    weatherCard(`${weather.location.name}, ${weather.location.country}`, `As of ${formattedDate}`, 'weather-summary', `${weather.current.temp_f}° F`, highLow);
+
+
+    weatherCard(`${weather.location.name}, ${weather.location.country}`, content, highLow, null);
 }
+// , `As of ${formattedDate}`, 'weather-summary', `${weather.current.temp_f}° F`
