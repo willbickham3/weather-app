a131dcb4afead4bf23ae95dccf863cca130b7428
Will <willbickham3@gmail.com>
2023-08-12 14:18:21 -0400

    Add an array that hold location values

--- src/pageHandlers/loadPage.js
+++ src/pageHandlers/loadPage.js
@@ -2,7 +2,8 @@ import weatherSummary from "./weatherSummary";
 import todayForecast from "./todayForecast";
 
 // Pre-determined value for the location
-const predeterminedLocation = "Navarre, FL";
+const locations = ['Navarre, FL', 'Boston, MA', 'Marietta, OH'];
+const predeterminedLocation = locations[Math.floor(Math.random() * locations.length)];
 
 // Call the functions with the predetermined location
 export default async function loadWeatherData() {
