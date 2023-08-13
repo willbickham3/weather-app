0c7d74c79d5ad478cb068d8bd5e8c3f32224c0d1
Will <willbickham3@gmail.com>
2023-08-13 16:26:53 -0400

    Create divs to append to header; move content divs around

--- src/pageHandlers/weatherSummary.js
+++ src/pageHandlers/weatherSummary.js
@@ -11,10 +11,12 @@ export default async function weatherSummary(search) {
     let newdate = new Date(weather.current.last_updated);
     let formattedDate = format(newdate, "HH:mm");
 
+
     let content = createAnElement('div', 'weather-summary', null, '');
-    const one = createAnElement('div', 'content-container', null, `As of ${formattedDate}`);
-    const two = createAnElement('div', 'content-container', null, `${weather.current.temp_f}° F`);
-    const three = createAnElement('div', 'content-container', null, 'this will have content');
+    const one = createAnElement('div', 'content', 'current-temperature', `${weather.current.temp_f}° F`);
+    const two = createAnElement('div', 'content', 'current-condition', `${weather.current.condition.text}`);
+    const three = createAnElement('img', 'content-container', null, '');
+    three.src = weather.current.condition.icon
 
     content.append(one, two, three)
 
@@ -23,7 +25,14 @@ export default async function weatherSummary(search) {
     if (weather.location.country == 'United States of America')
     {weather.location.country = weather.location.region};
 
+    let weatherHeader = createAnElement('div', 'weather-header', null, '');
+    let weatherLocation = createAnElement('div', 'weather-location', null, `${weather.location.name}, ${weather.location.country}`);
+    let asOfTimer = createAnElement('div', 'asOf', null, `As of ${formattedDate}`);
 
-    weatherCard(`${weather.location.name}, ${weather.location.country}`, content, highLow, null);
+    weatherHeader.append(weatherLocation, asOfTimer);
+
+    weatherCard(weatherHeader, content, highLow, null);
+
+
 }
 // , `As of ${formattedDate}`, 'weather-summary', `${weather.current.temp_f}° F`
