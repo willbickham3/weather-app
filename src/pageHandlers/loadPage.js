a855d9cc38256e190ac483057788b375160047d0
Will <willbickham3@gmail.com>
2023-08-13 16:25:22 -0400

    Add additional location to array

--- src/pageHandlers/loadPage.js
+++ src/pageHandlers/loadPage.js
@@ -2,7 +2,7 @@ import weatherSummary from "./weatherSummary";
 import todayForecast from "./todayForecast";
 
 // Pre-determined value for the location
-const locations = ['Navarre, FL', 'Boston, MA', 'Marietta, OH'];
+const locations = ['Navarre, FL', 'Boston, MA', 'Marietta, OH', 'Fort Walton Beach, FL'];
 const predeterminedLocation = locations[Math.floor(Math.random() * locations.length)];
 
 // Call the functions with the predetermined location
