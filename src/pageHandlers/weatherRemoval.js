8eca6b22de9e31a3e1e34b803f0136608c4278f4
Will <willbickham3@gmail.com>
2023-08-17 19:30:04 -0400

    Moved file

--- src/pageHandlers/weatherRemoval.js
+++
@@ -1,7 +0,0 @@
-export default function weatherRemoval() {
-    const weather = document.querySelectorAll('.sub-container');
-    console.log(weather);
-    weather.forEach(container => {
-        container.remove();
-    })
-}
