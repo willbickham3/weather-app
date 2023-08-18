ffedc931be00b9b8d596e00d77d9e0ea0d20680a
Will <willbickham3@gmail.com>
2023-08-17 23:15:41 -0400

    Add switch for deciding moon svg to use

--- src/pageHandlers/moreWeather.js
+++ src/pageHandlers/moreWeather.js
@@ -7,15 +7,67 @@ import humidity from "../images/svgs/humidity.svg";
 import uvIndex from "../images/svgs/uv-index.svg";
 import visibility from "../images/svgs/visibility.svg";
 import wind from "../images/svgs/wind.svg";
+import firstquarter from "../images/svgs/moon-phases/firstquarter.svg";
+import waxingcrescent from "../images/svgs/moon-phases/waxingcrescent.svg";
+import full from "../images/svgs/moon-phases/full.svg";
+import waningcrescent from "../images/svgs/moon-phases/waningcrescent.svg";
+import lastquarter from "../images/svgs/moon-phases/lastquarter.svg";
+import newmoon from "../images/svgs/moon-phases/newmoon.svg";
+import waninggibbous from "../images/svgs/moon-phases/waninggibbous.svg";
+import waxinggibbous from "../images/svgs/moon-phases/waxinggibbous.svg";
 
 
 export default async function moreWeather(search) {
     let weather = await getForecast(search);
 
+
+    // 3 main categories that are arguments for weatherCard at the bottom
     const heading = createAnElement('div', 'more-heading', null, '');
     const content = createAnElement('div', 'more-weather', null, '');
     const footing = createAnElement('div', 'more-footing', null, '');
 
+
+    // This sets moonphase to one of the SVG's in images for use in Sections object
+    let moonphase = weather.forecast.forecastday[0].astro.moon_phase;
+    switch (moonphase) {
+        case 'Waxing Crescent':
+            moonphase = waxingcrescent;
+            break
+
+        case 'Waning Crescent':
+            moonphase = waningcrescent;
+            break
+
+        case 'New Moon':
+            moonphase = newmoon;
+            break
+
+        case 'Full Moon':
+            moonphase = full;
+            break
+        
+        case 'Waning Gibbous':
+            moonphase = waninggibbous;
+            break
+
+        case 'Last Quarter':
+            moonphase = lastquarter;
+            break
+
+        case 'First Quarter':
+            moonphase = firstquarter;
+            break
+
+        case 'Waxing Gibbous':
+            moonphase = waxinggibbous;
+            break
+        
+        default:
+            moonphase = full;
+    }   
+
+
+    // moreContent and moreWeatherContent are the builders for the main part of the weathercard
     const moreContent = createAnElement('div', 'more-stuff', null, '');
 
     function moreWeatherContent(content, number, srcURL) {
@@ -26,77 +78,28 @@ export default async function moreWeather(search) {
         return { img, weathercontent, num }
     }
     
+    // This is setting all the parameters for weatherCard. More can be added as needed or taken out or moved around!
     const sections = [
         {name: 'high-low', content: 'High/Low', number: `${weather.forecast.forecastday[0].day.maxtemp_f}/${weather.forecast.forecastday[0].day.mintemp_f}`, imgSrc: `${thermometer}`},
         {name: 'wind', content: 'Wind', number: `${weather.current.gust_mph} mph ${weather.current.wind_dir}`, imgSrc: `${wind}`},
         {name: 'humidity', content: 'Humidity', number: `${weather.current.humidity}`, imgSrc: `${humidity}`},
-        // {name: 'dewpoint', content: 'Dew Point', number: `${weather.}`, imgSrc: `${}`},
         {name: 'pressure', content: 'Air Pressure', number: `${weather.current.pressure_in} in.`, imgSrc: `${barometer}`},
         {name: 'uv-index', content: 'UV Index', number: `${weather.current.uv}`, imgSrc: `${uvIndex}`},
         {name: 'visibility', content: 'Visibility', number: `${weather.current.vis_miles} mi.`, imgSrc: `${visibility}`},
-        {name: 'moon-phase', content: 'Moon Phase', number: `${weather.forecast.forecastday[0].astro.moon_phase}`, imgSrc: `${thermometer}`}
+        {name: 'moon-phase', content: 'Moon Phase', number: `${weather.forecast.forecastday[0].astro.moon_phase}`, imgSrc: `${moonphase}`}
     ]
 
     for (const section of sections) {
-        let meatContainer = createAnElement('div', 'more-content', `${section.name}`, '');
+        let container = createAnElement('div', 'more-content', `${section.name}`, '');
 
-        let meat = moreWeatherContent(section.content, section.number, section.imgSrc);
+        let content = moreWeatherContent(section.content, section.number, section.imgSrc);
 
-        meatContainer.append(meat.img, meat.weathercontent, meat.num);
+        container.append(content.img, content.weathercontent, content.num);
 
-        moreContent.append(meatContainer);
+        moreContent.append(container);
     }
 
-    
-
-
     const sunStuff = createAnElement('div', 'sun-stuff', null, '');
-
-    // Appended to sunstuff
-    // const temp = createAnElement('div', 'more-temp', null, '');
-    // const feelsLike = createAnElement('div', 'feelsLike', null, `Feels like ${weather.current.feelslike_f}° F`);
-
-    // Wind Container
-    // const wind = createAnElement('div', 'wind', null, '');
-    // const windImg = createAnElement('img', 'more-img', null, '');
-    // const  = createAnElement('div', 'more-content', null, '');
-    // const  = createAnElement('div', 'more-number', null, '');
-
-    // Humidity Container
-    // const humidity = createAnElement('div', 'humidity', null, '');
-    // const  = createAnElement('img', 'more-img', null, '');
-    // const  = createAnElement('div', 'more-content', null, '');
-    // const  = createAnElement('div', 'more-number', null, '');
-
-    // Dew Point Container
-    // const dewPoint = createAnElement('div', 'dew-point', null, '');
-    // const  = createAnElement('img', 'more-img', null, '');
-    // const  = createAnElement('div', 'more-content', null, '');
-    // const  = createAnElement('div', 'more-number', null, '');
-
-    // Pressure Container
-    // const pressure = createAnElement('div', 'pressure', null, '');
-    // const  = createAnElement('img', 'more-img', null, '');
-    // const  = createAnElement('div', 'more-content', null, '');
-    // const  = createAnElement('div', 'more-number', null, '');
-
-    // UV Index Container
-    // const uvIndex = createAnElement('div', 'uvIndex', null, '');
-    // const  = createAnElement('img', 'more-img', null, '');
-    // const  = createAnElement('div', 'more-content', null, '');
-    // const  = createAnElement('div', 'more-number', null, '');
-
-
-
-    // const  = createAnElement('div', 'visibility', null, '');
-    // const  = createAnElement('div', 'moon-phase', null, '');
-
-
-
-    // temp.append(feelsLike);
-
-    // sunStuff.append();
-    // moreContent.append();
 
     content.append(sunStuff, moreContent)
     weatherCard(heading, content, '', 'more-weather');
